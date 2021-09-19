57cc14d7748b2c3898685550b5d181daae9c8fe6
eloyms262 <eloyMs26@gmail.com>
2021-09-19 13:04:47 +0800

    first commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+//api/new-meetup
+// POST
+async function handler(req, res) {
+    if (req.method === 'POST'){
+        const data = req.body;
+
+        const client = await MongoClient.connect(
+            'mongodb+srv://Programmer:lmoments262@api.utd89.mongodb.net/meetups?retryWrites=true&w=majority'
+            );
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups'); 
+        const result = await meetupsCollection.insertOne(data); 
+
+        console.log(result);
+
+        client.close;
+
+        res.status(201).json({ message: 'data inserted' });
+
+    }
+}
+
+export default handler;
